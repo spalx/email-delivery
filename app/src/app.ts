2dd3a5d5d6fabde566cd59346e8add8d68c1bc6e
Artavazd Barseghyan <artavazd1@gmail.com>
2025-08-22 22:12:23 +0200

    transport updates

--- app/src/app.ts
+++ app/src/app.ts
@@ -2,13 +2,15 @@ import { IAppPkg, AppRunPriority } from 'app-life-cycle-pkg';
 import { EmailDeliveryAction, SendEmailDTO } from 'email-delivery-pkg';
 import { transportService, TransportAdapterName, CorrelatedRequestDTO } from 'transport-pkg';
 import { KafkaTransportAdapter } from 'kafka-transport-adapter';
+import { HTTPTransportAdapter } from 'http-transport-adapter';
 
 import SendEmailCommand from './commands/email/send-email.command';
 import emailOpWorker from '@/queues/workers/email-op.worker';
 
 class App implements IAppPkg {
   async init(): Promise<void> {
-    transportService.registerTransport(TransportAdapterName.Kafka, new KafkaTransportAdapter('email-delivery'));
+    transportService.registerTransport(TransportAdapterName.Kafka, new KafkaTransportAdapter('kafka:9092', 'email-delivery'));
+    transportService.registerTransport(TransportAdapterName.HTTP, new HTTPTransportAdapter(3020));
     transportService.transportsReceive(EmailDeliveryAction.SendEmail, async (data: CorrelatedRequestDTO) => {
       this.sendEmailMessageReceived(data);
     });
